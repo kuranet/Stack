bda0deb882c998391f442775592f62b0d2ab1ab3
Ann <anna.kurennaya38@gmail.com>
2020-09-27 10:45:29 +0300

    Create project and add platform moving

---
+++ Stackk/Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public Vector3 start;
+    public Vector3 end;
+
+    bool isMoving = true;
+
+    float speed = 0.1f;
+    Vector3 target;
+
+    void Start()
+    {
+        target = end;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            if(transform.position != target)
+                transform.position = Vector3.MoveTowards(transform.position, target, speed);
+            else
+            {
+                if (target == end)
+                    target = start;
+                else
+                    target = end;
+            }
+        } 
+    }
+}
---
+++ Stackk/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject platformPrefab;
+
+    public void SpawnPlatforn()
+    {
+        Instantiate(platformPrefab);
+    }
+}
